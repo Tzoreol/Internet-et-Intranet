0328a490929bacc13fb024ea5dcacb53bedde60e
Tzoreol <tzoreol@gmail.com>
2024-01-28 15:49:32 +0100

    Correction TD 3

--- Javascript avancé/TD3/id.class.js
+++ Javascript avancé/TD3/id.class.js
@@ -1,3 +1,31 @@
 class Id {
-    
+    constructor(firstname, lastname, dateOfBirth, gender, country) {
+        this._firstname = firstname;
+        this._lastname = lastname;
+        this._dateOfBirth = dateOfBirth;
+        this._gender = gender;
+        this._country = country;
+    }
+
+    get firstname() {
+        return this._firstname
+    }
+
+    get lastname() {
+        return this._lastname
+    }
+
+    get dateOfBirth() {
+        let ts = Date.parse(this._dateOfBirth)
+        let date = new Date(ts)
+        return date.getUTCDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    }
+
+    get gender() {
+        return this._gender
+    }
+
+    get country() {
+        return this._country
+    }
 }
